f942eb922129e3442846d1ca16de8c3f9d889591
m <mdaly@lifeimage.com>
2016-08-19 11:33:21 -0400

    use local file

--- syscall-fuzzer.cpp
+++ syscall-fuzzer.cpp
@@ -5,43 +5,59 @@
 #include <sys/syscall.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <signal.h>
+#include <fcntl.h>
+// #include <signal.h>
+// #include <stdio.h>
 
-const int not_impl[] = {174, 177, 178, 180, 181, 182, 183, 184, 185, 214, 215, 236};
+void invoke(int sysno, int fd) {
+    const int size = 5;
+    char buf[size];
+
+    switch (sysno) {
+
+    case SYS_read: // (unsigned int fd,	char __user * buf, size_t count)
+        syscall(SYS_read, fd, buf, size);
+        printf("%s\n", buf);
+        break;
+
+    case SYS_write: // (unsigned int fd, const char __user * buf, size_t count)
+        syscall(SYS_write, fd, "boo!\n", size);
+        break;
+
+    case SYS_open: // (const char __user * filename, int flags, umode_t mode)
+        break;
+
+    case SYS_close: break;
+    case SYS_stat: break;
+    case SYS_fstat: break;
+    case SYS_lstat: break;
+    case SYS_poll: break;
+    default: break;
+    }
+}
+
+// non-implemented system call numbers
+const int skip[] = {174, 177, 178, 180, 181, 182, 183, 184, 185, 214, 215, 236};
+auto start = std::begin(skip);
+auto end = std::end(skip);
 
 int main() {
-    int sysno;
-    bool not_allowed;
-
-    // // TODO: map sysnos to default args
-    // int[] sysnos = {};
-    // for (int i = 0; i <= 300; i++) {
-    //   not_allowed = std::any_of(std::begin(not_impl), std::end(not_impl), [&](int i) {
-    //       return i == sysno;
-    //     });
-
-    //   if (not_allowed) {
-    //     continue;
-    //   } else {
-    //     sysnos[i] = i;
-    //   }
-    // }
-
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> dis(0, 313);
 
-    auto beg = std::begin(not_impl);
-    auto end = std::end(not_impl);
+    int fd = open("./file.txt", O_RDWR | O_APPEND);
+    int sysno;
 
     for (int i = 0; i < 100; i++) {
         do {
             sysno = dis(gen);
-            not_allowed = std::any_of(beg, end, [&](int i) { return i == sysno; });
-        } while (not_allowed);
+        } while (std::any_of(start, end, [&](int j) { return j == sysno; }));
 
-        std::cout << sysno << ' ';
+        // FIXME: this should happen in a child process
+        invoke(sysno, fd);
     }
 
+    close(fd);
     return 0;
 }
