78f0ffe27dae8085118dcfc77281c73051a94066
m <mdaly@lifeimage.com>
2016-08-19 06:46:15 -0400

    fix indent style

--- syscall-fuzzer.cpp
+++ syscall-fuzzer.cpp
@@ -1,4 +1,3 @@
-// g++ -std=c++1 -Wall -g -o syscall-fuzzer syscall-fuzzer.cpp && ./syscall-fuzzer
 #include <algorithm>
 #include <iostream>
 #include <random>
@@ -10,41 +9,39 @@
 
 const int not_impl[] = {174, 177, 178, 180, 181, 182, 183, 184, 185, 214, 215, 236};
 
-
 int main() {
-  int sysno;
-  bool not_allowed;
+    int sysno;
+    bool not_allowed;
 
-  // // TODO: map sysnos to default args
-  // int[] sysnos = {};
-  // for (int i = 0; i <= 300; i++) {
-  //   not_allowed = std::any_of(std::begin(not_impl), std::end(not_impl), [&](int i) {
-  //       return i == sysno;
-  //     });
+    // // TODO: map sysnos to default args
+    // int[] sysnos = {};
+    // for (int i = 0; i <= 300; i++) {
+    //   not_allowed = std::any_of(std::begin(not_impl), std::end(not_impl), [&](int i) {
+    //       return i == sysno;
+    //     });
 
-  //   if (not_allowed) {
-  //     continue;
-  //   } else {
-  //     sysnos[i] = i;
-  //   }
-  // }
+    //   if (not_allowed) {
+    //     continue;
+    //   } else {
+    //     sysnos[i] = i;
+    //   }
+    // }
 
-  std::random_device rd;
-  std::mt19937 gen(rd());
-  std::uniform_int_distribution<> dis(0, 300);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, 313);
 
-  for (int i = 0; i < 100; i++) {
-    do {
-      sysno = dis(gen);
+    auto beg = std::begin(not_impl);
+    auto end = std::end(not_impl);
 
-      not_allowed = std::any_of(std::begin(not_impl), std::end(not_impl), [&](int i) {
-          return i == sysno;
-        });
-    } while (not_allowed);
+    for (int i = 0; i < 100; i++) {
+        do {
+            sysno = dis(gen);
+            not_allowed = std::any_of(beg, end, [&](int i) { return i == sysno; });
+        } while (not_allowed);
 
-    std::cout << sysno << ' ';
-  }
-  std::cout << std::endl;
+        std::cout << sysno << ' ';
+    }
 
-  return 0;
+    return 0;
 }
